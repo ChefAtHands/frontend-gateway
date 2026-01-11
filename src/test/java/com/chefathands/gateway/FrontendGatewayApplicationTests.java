c890901f26b3396830ba5536498552742f07f2c9
AnejT <125371242+AnejT@users.noreply.github.com>
2026-01-11 11:37:16 +0100

    testi

--- src/test/java/com/chefathands/gateway/FrontendGatewayApplicationTests.java
+++ src/test/java/com/chefathands/gateway/FrontendGatewayApplicationTests.java
@@ -1,3 +1,12 @@
-public class FrontendGatewayApplicationTests {
-    
+package com.chefathands.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 }
